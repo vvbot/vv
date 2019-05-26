f64a17161fd182d679ad191c92d422c4ad58fc7b
Axel Greavette <vultureavatar@gmail.com>
2019-05-25 21:19:53 -0400

    GAHHH IM SO DUMB

--- src/commands/new-update.js
+++ src/commands/new-update.js
@@ -5,8 +5,11 @@ module.exports = {
     usage: "[TITLE] [DESCRIPTION]",
     cooldown: 5,
     adminOnly: true,
-    async execute(message, args, client, logger) {
-        client.sql.query(`INSERT INTO updates (title, description) VALUES (${args[0].replace(/"/g).toString()}, ${args[1].replace(/"/g).toString()})`, (error, rows, fields) => {
+    async execute(message, args, client, logger) {        
+        args = args.join(" ");
+        let title = args.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+        let description = args.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[1];
+        client.sql.query(`INSERT INTO updates (title, description) VALUES ("${title}", "${description}")`, (error, rows, fields) => {
             if(error) throw error;
             return message.channel.send("Successfully pushed update.");
         });
