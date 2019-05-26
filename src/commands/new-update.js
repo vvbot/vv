3cabfa993a4e00d03b4ab95ed1953584dfdf6978
Axel Greavette <vultureavatar@gmail.com>
2019-05-25 21:34:33 -0400

    frick me

--- src/commands/new-update.js
+++ src/commands/new-update.js
@@ -6,10 +6,8 @@ module.exports = {
     cooldown: 5,
     adminOnly: true,
     async execute(message, args, client, logger) {        
-        args = args.join(" ");
-        let title = args.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
-        let description = args.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[1];
-        client.sql.query(`INSERT INTO updates (title, description) VALUES ("${title}", "${description}")`, (error, rows, fields) => {
+        args = args.join(" ").split('" "');
+        client.sql.query(`INSERT INTO updates (title, description) VALUES ("${args[0].replace(/"/, "")}", "${args[1].replace(/"/, "")}")`, (error, rows, fields) => {
             if(error) throw error;
             return message.channel.send("Successfully pushed update.");
         });
