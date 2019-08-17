a6b0dee86684ae3545751146a100167374281e5f
Axel Greavette <vultureavatar@gmail.com>
2019-08-17 12:34:37 -0400

    slight fox

--- src/commands/delete-tag.js
+++ src/commands/delete-tag.js
@@ -7,9 +7,9 @@ module.exports = {
     cooldown: 5,
     execute(message, args, client, logger) {
         args = args.join(" ").split('" "');
-        client.sql.query(`SELECT * FROM tags WHERE tagName=${args[0].replace(/"/g, "")}`, (error, rows, fields) => {
-            if(!message.author.id == rows[0].authorID && !client.config.bot.admins.includes(message.author.id)) return message.channel.send("You do not have the permissions to delete that tag.");
-            else client.sql.query(`DELETE FROM tags WHERE tagName=${args[0].replace(/"/g, "")}`, (error, rows, fields) => {
+        client.sql.query(`SELECT * FROM tags WHERE tagName = "${args[0].replace(/"/g, "")}"`, (error, rows, fields) => {
+            if(!message.author.id == rows[0].authorID && !client.config.bot.admins.includes(message.author.id)) return message.channel.send("You do not have the permissions required to delete that tag.");
+            else client.sql.query(`DELETE FROM tags WHERE tagName = "${args[0].replace(/"/g, "")}"`, (error, rows, fields) => {
                 message.channel.send("👌")
             })
         });
