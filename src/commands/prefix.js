fcbad800df8f385cd643150a9aafbf8d424ad8e7
Axel Greavette <vultureavatar@gmail.com>
2020-01-05 15:34:14 -0500

    Wuick

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -4,6 +4,6 @@ module.exports = {
     args: false,
     usage: "",
     execute(message, args, client, logger) {
-        message.channel.send(`You can utilise functions through me by using either the **${client.prefix}** or **${client.user}** prefix`)
+        message.channel.send(`You can utilise functions through me by using either the **${client.prefix}** or **${client.user}** prefix, or set one for yourself with **${client.prefix}settings set prefix [prefix]** or **${client.user}settings set prefix [prefix]**`)
     }
 }
